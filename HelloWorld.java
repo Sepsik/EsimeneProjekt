8ce12142f8a51970575fd23f504355937a9a1fec
Anita Sepp <anitasepp@hotmail.com>
2016-10-05 23:11:45 +0300

    HelloWorld lisamine

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by anitasepp on 01/10/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
